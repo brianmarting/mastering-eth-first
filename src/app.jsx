447776c333259c384e5c1d5be11a58937c350b88
brianmarting <brian.marting@hotmail.com>
2021-04-28 23:54:23 +0200

    init comm

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component, useEffect, useState } from "react";
+
+const App = () => {
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div className='App'>
+        
+    </div>
+  );
+}
+
+export default App;
